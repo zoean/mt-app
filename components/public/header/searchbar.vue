f03bb36f158cef9635dfb60dde9a5eeb795da2f5
zoe <webxly@gmail.com>
2019-12-01 22:53:45 +0800

    products end

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -21,16 +21,18 @@
               v-if="isHotPlace">
             <dt>热门搜索</dt>
             <dd v-for="(item,idx) in $store.state.home.hotPlace.slice(0,5)"
-                :key="idx">{{item.name}}</dd>
+                :key="idx">
+              <a :href="'/products?keyword='+encodeURIComponent(item.name)">{{item.name}}</a>
+            </dd>
           </dl>
           <dl class="searchList"
               v-if="isSearchList">
             <dd v-for="(item,idx) in searchList"
-                :key="idx">{{item.name}}</dd>
+                :key="idx"><a :href="'/products?keyword='+encodeURIComponent(item.name)">{{item.name}}</a></dd>
           </dl>
         </div>
         <p class="suggest">
-          <a href=""
+          <a :href="'/products?keyword='+encodeURIComponent(item.name)"
              v-for="(item,idx) in $store.state.home.hotPlace.slice(0,5)"
              :key="idx">{{item.name}}</a>
         </p>
